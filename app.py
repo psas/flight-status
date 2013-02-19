e0dea1a7c89560ef3a5482692543d5e26f68a74f
Nathan Bergey <nathan.bergey@gmail.com>
2013-02-18 18:01:22 -0800

    placeholder login screen

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
