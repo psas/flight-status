0c3045799036f8b23ffcc3b8a51e0ffc86918093
Nathan Bergey <nathan.bergey@gmail.com>
2013-02-18 17:43:22 -0800

    rebasing style

--- app.py
+++ app.py
@@ -6,61 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    systems = views.show_systems()
-    return render_template('index.html',
-                           systems=systems)
-
-@app.route('/manage', methods=['GET', 'POST'])
-def mange():
-    if request.method == 'POST':
-        if request.form['key'] == "NEW":
-            psas.add_system_from_form(request.form)
-        elif "DELETE" in request.form['key']:
-            key = request.form['key'][6:]
-            psas.delete_system(key)
-        else:
-            psas.update_system_from_from(request.form)
-    systems = views.show_systems()
-    tags = views.show_tags()
-    return render_template('manage.html',
-                           systems=systems,
-                           tags=tags)
-
-@app.route('/manage/<system_key>', methods=['GET', 'POST'])
-def manage_system(system_key):
-    if request.method == 'POST':
-        if request.form['key'] == "NEW":
-            psas.add_part_from_form(request.form, system_key)
-        elif "REM" in request.form['key']:
-            key = request.form['key'][3:]
-            psas.remove_part(system_key, key)
-        elif request.form['key'] == "ADD":
-            psas.add_part_from_form(request.form, system_key)
-        else:
-            psas.update_part_from_form(request.form)
-
-    system = views.show_system(system_key)
-    parts = views.show_parts(system_key)
-    all_parts = views.list_all_parts()
-    return  render_template('system_manage.html',
-                            system=system,
-                            parts=parts,
-                            allparts=all_parts,
-                            softdelete="True")
-
-@app.route('/parts', methods=['GET', 'POST'])
-def parts():
-    if request.method == 'POST':
-        if request.form['key'] == "NEW":
-            psas.add_part_from_form(request.form)
-        elif "DELETE" in request.form['key']:
-            key = request.form['key'][6:]
-            psas.delete_part(key)
-        else:
-            psas.update_part_from_form(request.form)
-    parts = views.show_all_parts()
-    return render_template('part_manage.html',
-                            parts=parts)
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
